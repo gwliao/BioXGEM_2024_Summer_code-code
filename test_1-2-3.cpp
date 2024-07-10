9222102807cdcde66e93fb41a7cc950d2508c768
guanweiliao <gwliao.bt13@nycu.edu.tw>
2024-07-10 23:31:31 +0800

    creative file

---
+++ test_1-2-3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a ;              // 整數類型
+    cin >> a;
+
+    cout << "Hello world " << a << endl;
+    cout << a << endl;
+
+ return 0;
+}
